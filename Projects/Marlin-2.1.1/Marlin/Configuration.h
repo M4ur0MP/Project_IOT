f9102383d690fe3c7c95016da0d7bb995d824fe7
MauroMP1 <mmartinezpastre@gmail.com>
2023-09-02 08:22:05 -0300

    Update

--- Projects/Marlin-2.1.1/Marlin/Configuration.h
+++ Projects/Marlin-2.1.1/Marlin/Configuration.h
@@ -135,7 +135,7 @@
 // #define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Anet A8 - BBT_SKR1.4T V_1.2"
+#define CUSTOM_MACHINE_NAME "Anet A8 - BBT_SKR1.4T V_1.3"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
@@ -1971,7 +1971,7 @@
 // After a runout is detected, continue printing this length of filament
 // before executing the runout script. Useful for a sensor at the end of
 // a feed tube. Requires 4 bytes SRAM per sensor, plus 4 bytes overhead.
-#define FILAMENT_RUNOUT_DISTANCE_MM 10
+#define FILAMENT_RUNOUT_DISTANCE_MM 7
 
 #ifdef FILAMENT_RUNOUT_DISTANCE_MM
 // Enable this option to use an encoder disc that toggles the runout pin
